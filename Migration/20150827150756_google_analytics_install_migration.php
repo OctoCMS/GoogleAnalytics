e3ccd32e1dfaca1ca0a61c88c3813eea13c9c368
Dan Cryer <dan@block8.co.uk>
2015-08-27 17:00:23 +0100

    Migrations

---
+++ Migration/20150827150756_google_analytics_install_migration.php
@@ -0,0 +1,118 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class GoogleAnalyticsInstallMigration extends AbstractMigration
+{
+    public function up()
+    {
+        // Create tables:
+        $this->createGaPageView();
+        $this->createGaSummaryView();
+        $this->createGaTopPage();
+
+        // Add foreign keys:
+        $table = $this->table('ga_top_page');
+
+        if (!$table->hasForeignKey('page_id') && $this->hasTable('page')) {
+            $table->addForeignKey('page_id', 'page', 'id', ['delete' => 'SET_NULL', 'update' => 'CASCADE']);
+            $table->save();
+        }
+    }
+
+    protected function createGaPageView()
+    {
+        $table = $this->table('ga_page_view', ['id' => false, 'primary_key' => ['id']]);
+
+        if (!$this->hasTable('ga_page_view')) {
+            $table->addColumn('id', 'integer', ['signed' => false, 'null' => false]);
+            $table->create();
+        }
+
+        if (!$table->hasColumn('date')) {
+            $table->addColumn('date', 'date', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('updated')) {
+            $table->addColumn('updated', 'datetime', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('value')) {
+            $table->addColumn('value', 'integer', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('metric')) {
+            $table->addColumn('metric', 'string', ['limit' => 250, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasIndex(['metric', 'date'])) {
+            $table->addIndex(['metric', 'date'], ['unique' => true]);
+        }
+
+        $table->save();
+    }
+
+    protected function createGaSummaryView()
+    {
+        $table = $this->table('ga_summary_view', ['id' => false, 'primary_key' => ['id']]);
+
+        if (!$this->hasTable('ga_summary_view')) {
+            $table->addColumn('id', 'integer', ['signed' => false, 'null' => false]);
+            $table->create();
+        }
+
+        if (!$table->hasColumn('updated')) {
+            $table->addColumn('updated', 'datetime', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('value')) {
+            $table->addColumn('value', 'integer', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('metric')) {
+            $table->addColumn('metric', 'string', ['limit' => 250, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasIndex(['metric'])) {
+            $table->addIndex(['metric'], ['unique' => true]);
+        }
+
+        $table->save();
+    }
+
+    protected function createGaTopPage()
+    {
+        $table = $this->table('ga_top_page', ['id' => false, 'primary_key' => ['id']]);
+
+        if (!$this->hasTable('ga_top_page')) {
+            $table->addColumn('id', 'integer', ['signed' => false, 'null' => false]);
+            $table->create();
+        }
+
+        if (!$table->hasColumn('updated')) {
+            $table->addColumn('updated', 'datetime', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('pageviews')) {
+            $table->addColumn('pageviews', 'integer', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('unique_pageviews')) {
+            $table->addColumn('unique_pageviews', 'integer', ['null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('uri')) {
+            $table->addColumn('uri', 'string', ['limit' => 250, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasColumn('page_id')) {
+            $table->addColumn('page_id', 'char', ['limit' => 5, 'null' => true, 'default' => null]);
+        }
+
+        if (!$table->hasIndex(['uri'])) {
+            $table->addIndex(['uri'], ['unique' => true]);
+        }
+
+        $table->save();
+    }
+}
